e38710836c43684b816bbc55f23bc84ad5dcea31
lthurler <leothurler@gmail.com>
2023-01-19 18:36:13 -0300

    fim da parte mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,16 +67,107 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 });
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+      e.preventDefault()
+
+      // Check if the field has a value
+      if(contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+          // Add and remove color
+          contactMessage.classList.remove('color-blue')
+          contactMessage.classList.add('color-red')
+          // Show message
+          contactMessage.textContent = 'Preencha todos os campos 📩'
+      }else {
+          // serviceID - templateID - #form - publicKey
+          emailjs.sendForm('service_vhr6jx1','template_b6335rj','#contact-form','zv4cDFntr7h6rvv2m') 
+              .then(() => {
+                  // Show message and add color
+                  contactMessage.classList.add('color-blue')
+                  contactMessage.textContent = 'Email enviado ✅'
+
+                  // Remove message after five seconds
+                  setTimeout(() => {
+                      contactMessage.textContent = ''
+                  }, 5000)
+                
+              }, (error) => {
+                  alert('OPA! ALGO FALHOU...', error)
+              })
+
+            // To clear the input field
+            contactName.value = ''
+            contactEmail.value = ''
+            contactProject.value = ''
+      }
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
